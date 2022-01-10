07aab6b47e0ea7b811dfd0faa3241b9cfcb386ab
nagababu@usf.edu <nagababu@usf.edu>
2022-01-10 13:05:20 -0500

    Modifications

--- Assignment_1_Spring_2022/Program.cs
+++ Assignment_1_Spring_2022/Program.cs
@@ -20,9 +20,9 @@ namespace DIS_Assignmnet1_SPRING_2022
             Console.WriteLine();
 
             //Question 2:
-            string[] word1 = new string[]{"ab","c"};
-            string[] word2 =new string[]{"a","bc"};
-            bool flag = ArrayStringsAreEqual(word1,word2);
+            string[] bulls_string1 = new string[]{"Marshall", "Student","Center"};
+            string[] bulls_string2 = new string[]{"MarshallStudent", "Center"};
+            bool flag = ArrayStringsAreEqual(bulls_string1, bulls_string2);
             Console.WriteLine("Q2");
             if (flag)
             {
@@ -35,32 +35,32 @@ namespace DIS_Assignmnet1_SPRING_2022
             Console.WriteLine();
 
             //Question 3:
-            int[] nums = new int[] { 1, 2, 3, 2 };
-            int unique_sum = SumOfUnique(nums);
+            int[] bull_bucks = new int[] { 1, 2, 3, 2 };
+            int unique_sum = SumOfUnique(bull_bucks);
             Console.WriteLine("Q3:");
             Console.WriteLine("Sum of Unique Elements in the array are :{0}", unique_sum);
             Console.WriteLine();
 
 
             //Question 4:
-            int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] bulls_grid = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
             Console.WriteLine("Q4:");
-            int diagSum = DiagonalSum(matrix);
-            Console.WriteLine("The sum of diagonal elements are {0}:", diagSum);
+            int diagSum = DiagonalSum(bulls_grid);
+            Console.WriteLine("The sum of diagonal elements in the bulls grid is {0}:", diagSum);
             Console.WriteLine();
 
             //Question 5:
             Console.WriteLine("Q5:");
-            String word3 = "aiohn";
+            String bulls_string = "aiohn";
             int[] indices = { 3, 1, 4, 2, 0 };
-            String rotated_string = RestoreString(word3, indices);
+            String rotated_string = RestoreString(bulls_string, indices);
             Console.WriteLine("The  Final string after rotation is ", rotated_string);
             Console.WriteLine();
 
             //Quesiton 6:
-            string word4 ="abcdefd";
-            char ch ='d';
-            string reversed_string = ReversePrefix(word4, ch);
+            string bulls_string6 = "mumacollegeofbusiness";
+            char ch ='c';
+            string reversed_string = ReversePrefix(bulls_string6, ch);
             Console.WriteLine("Q6:");
             Console.WriteLine("Resultant string are reversing the prefix:{0}", reversed_string);
             Console.WriteLine();
@@ -72,8 +72,8 @@ namespace DIS_Assignmnet1_SPRING_2022
         Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
 
         Example 1:
-        Input: s = "lightcodeisacommunityforcoders"
-        Output: "lghtcdscmmntyfrcdrs"
+        Input: s = "MumaCollegeofBusiness"
+        Output: "MmCllgfBsnss"
 
         Example 2:
         Input: s = "aeiou"
@@ -105,23 +105,23 @@ namespace DIS_Assignmnet1_SPRING_2022
        Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
        A string is represented by an array if the array elements concatenated in order forms the string.
        Example 1:
-       Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+       Input: : bulls_string1 = ["Marshall", "Student",”Center”], : bulls_string2 = ["MarshallStudent ", "Center"]
        Output: true
        Explanation:
-       word1 represents string "ab" + "c" -> "abc"
-       word2 represents string "a" + "bc" -> "abc"
+       word1 represents string "marshall" + "student" + “center” -> "MarshallStudentCenter "
+       word2 represents string "MarshallStudent" + "Center" -> "MarshallStudentCenter"
        The strings are the same, so return true.
        Example 2:
-       Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+       Input: word1 = ["Zimmerman", "School", ”of Advertising”, ”and Mass Communications”], word2 = ["Muma", “College”,"of”, “Business"]
        Output: false
 
        Example 3:
-       Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
+       Input: word1  = ["University", "of", "SouthFlorida"], word2 = ["UniversityofSouthFlorida"]
        Output: true
        </summary>
        */
 
-        private static bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        private static bool ArrayStringsAreEqual(string[] bulls_string1, string[] bulls_string2)
         {
             try
             {
@@ -137,24 +137,24 @@ namespace DIS_Assignmnet1_SPRING_2022
         }
         /*
         <summary> 
-       You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+       You are given an integer array bull_bucks. The unique elements of an array are the elements that appear exactly once in the array.
        Return the sum of all the unique elements of nums.
        Example 1:
-       Input: nums = [1,2,3,2]
+       Input: bull_bucks = [1,2,3,2]
        Output: 4
        Explanation: The unique elements are [1,3], and the sum is 4.
        Example 2:
-       Input: nums = [1,1,1,1,1]
+       Input: bull_bucks = [1,1,1,1,1]
        Output: 0
        Explanation: There are no unique elements, and the sum is 0.
        Example 3:
-       Input: nums = [1,2,3,4,5]
+       Input: bull_bucks = [1,2,3,4,5]
        Output: 15
        Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
        </summary>
         */
 
-        private static int SumOfUnique(int[] nums)
+        private static int SumOfUnique(int[] bull_bucks)
         {
             try
             {
@@ -167,28 +167,28 @@ namespace DIS_Assignmnet1_SPRING_2022
                 throw;
             }
         }
-       /*
-       
-       <summary>
-      Given a square matrix mat, return the sum of the matrix diagonals.
-      Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+        /*
 
-      Example 1:
-      Input: mat = [[1,2,3],[4,5,6], [7,8,9]]
-      Output: 25
-      Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
-      Notice that element mat[1][1] = 5 is counted only once.
-      Example 2:
-      Input: mat = [[1,1,1,1], [1,1,1,1],[1,1,1,1], [1,1,1,1]]
-      Output: 8
-      Example 3:
-      Input: mat = [[5]]
-      Output: 5
-      </summary>
+        <summary>
+       Given a square matrix bulls_grid, return the sum of the matrix diagonals.
+       Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
 
-       */
+       Example 1:
+       Input: bulls_grid = [[1,2,3],[4,5,6], [7,8,9]]
+       Output: 25
+       Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+       Notice that element mat[1][1] = 5 is counted only once.
+       Example 2:
+       Input: bulls_grid = [[1,1,1,1], [1,1,1,1],[1,1,1,1], [1,1,1,1]]
+       Output: 8
+       Example 3:
+       Input: bulls_grid = [[5]]
+       Output: 5
+       </summary>
 
-        private static int DiagonalSum(int[,] matrix)
+        */
+
+        private static int DiagonalSum(int[,] bulls_grid)
         {
             try
             {
@@ -210,17 +210,17 @@ namespace DIS_Assignmnet1_SPRING_2022
         /*
          
         <summary>
-        Given a string s and an integer array indices of the same length.
-        The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+        Given a string bulls_string  and an integer array indices of the same length.
+        The string bulls_string  will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
         Return the shuffled string.
 
         Example 3:
-        Input: s = "aiohn", indices = [3,1,4,2,0]
+        Input: bulls_string  = "aiohn", indices = [3,1,4,2,0]
         Output: "nihao"
 
         */
 
-        private static string RestoreString(string s, int[] indices)
+        private static string RestoreString(string bulls_string, int[] indices)
         {
             try
             {
@@ -235,32 +235,32 @@ namespace DIS_Assignmnet1_SPRING_2022
 
         }
 
-      /*
-       <summary>
-      Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
-      For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
-      Return the resulting string.
+        /*
+         <summary>
+        Given a 0-indexed string bulls_string   and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+        For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+        Return the resulting string.
 
-      Example 1:
-      Input: word = "abcdefd", ch = "d"
-      Output: "dcbaefd"
-      Explanation: The first occurrence of "d" is at index 3. 
-      Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+        Example 1:
+        Input: bulls_string   = "mumacollegeofbusiness", ch = "c"
+        Output: "camumollegeofbusiness"
+        Explanation: The first occurrence of "d" is at index 3. 
+        Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
 
-      Example 2:
-      Input: word = "xyxzxe", ch = "z"
-      Output: "zxyxxe"
-      Explanation: The first and only occurrence of "z" is at index 3.
-      Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+        Example 2:
+        Input: bulls_string   = "xyxzxe", ch = "z"
+        Output: "zxyxxe"
+        Explanation: The first and only occurrence of "z" is at index 3.
+        Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
 
-      Example 3:
-      Input: word = "abcd", ch = "z"
-      Output: "abcd"
-      Explanation: "z" does not exist in word.
-      You should not do any reverse operation, the resulting string is "abcd".
-      */
+        Example 3:
+        Input: bulls_string   = "zimmermanschoolofadvertising", ch = "x"
+        Output: "zimmermanschoolofadvertising"
+        Explanation: "x" does not exist in word.
+        You should not do any reverse operation, the resulting string is "zimmermanschoolofadvertising".
+        */
 
-        private static string ReversePrefix(string word, char ch)
+        private static string ReversePrefix(string bulls_string6, char ch)
         {
             try
             {
